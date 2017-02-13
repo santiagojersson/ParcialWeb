d6d70b0c491926aa56a72992c78ef48ee62eda23
Santi <santiagojersson@gmail.com>
2017-02-12 22:19:44 -0500

    Agregada barra de Slider

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var misDatos= angular.module('myShop',[]);
+var misDatos=angular.module('myShop', ['rzModule', 'ui.bootstrap']);
       misDatos.controller('cargarProductos',function($scope,$http){
         $scope.importar= function(){
           $http.get('./dataParcial.json').success(function(datos){
@@ -9,6 +9,23 @@ var misDatos= angular.module('myShop',[]);
         $scope.importar();
      
         
+
+});
+
+
+
+misDatos.controller('MainCtrl', function ($scope, $rootScope, $timeout, $modal) {
+	 $scope.slider_translate = {
+        minValue: 10000,
+        maxValue: 40000,
+        options: {
+            ceil: 60000,
+            floor: 0,
+            translate: function (value) {
+                return '$' + value;
+            }
+        }
+    };
 
 });
 
